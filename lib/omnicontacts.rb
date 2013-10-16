bfa5a643fb5083a5660a62cae3abe5f59b6acb82
rubytastic <voorruby@gmail.com>
2013-10-16 11:56:45 +0200

    replace hardcoded contacts path with global variable

--- lib/omnicontacts.rb
+++ lib/omnicontacts.rb
@@ -2,13 +2,16 @@ module OmniContacts
   
   VERSION = "0.3.5"
 
+  MOUNT_PATH = "/contacts/"
+
   autoload :Builder, "omnicontacts/builder"
   autoload :Importer, "omnicontacts/importer"
   autoload :IntegrationTest, "omnicontacts/integration_test"
 
   class AuthorizationError < RuntimeError
   end
-  
+
+
   def self.integration_test
     IntegrationTest.instance
   end
