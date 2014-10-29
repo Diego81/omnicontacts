f90f7be43bc4b15261d5d6ac04bc30270892495c
Vinh Nguyen Le <vinh.nguyenlexuan@asnet.com.vn>
2014-10-29 08:54:15 +0700

    Update hotmail.rb

--- lib/omnicontacts/importer/hotmail.rb
+++ lib/omnicontacts/importer/hotmail.rb
@@ -11,10 +11,10 @@ module OmniContacts
 
       def initialize app, client_id, client_secret, options ={}
         super app, client_id, client_secret, options
-        @auth_host = "login.live.com"
-        @authorize_path = "/oauth20_authorize.srf"
+        @auth_host = "oauth.live.com"
+        @authorize_path = "/authorize"
         @scope = options[:permissions] || "wl.signin, wl.basic, wl.birthday , wl.emails ,wl.contacts_birthday , wl.contacts_photos"
-        @auth_token_path = "/oauth20_token.srf"
+        @auth_token_path = "/token"
         @contacts_host = "apis.live.net"
         @contacts_path = "/v5.0/me/contacts"
         @self_path = "/v5.0/me"
