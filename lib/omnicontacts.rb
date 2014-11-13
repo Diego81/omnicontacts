acdb8bf010abfe6ff40c0369076ba29a5ba232d9
Daniel P. Clark <webmaster@6ftdan.com>
2014-11-13 13:39:26 -0500

    change mount path to /import

--- lib/omnicontacts.rb
+++ lib/omnicontacts.rb
@@ -2,7 +2,7 @@ module OmniContacts
   
   VERSION = "0.3.5"
 
-  MOUNT_PATH = "/contacts/"
+  MOUNT_PATH = "/import/"
 
   autoload :Builder, "omnicontacts/builder"
   autoload :Importer, "omnicontacts/importer"
