64a26bd04bd48d65147349c61cbccb6913966a53
Asma Tameem <asma@levoleague.com>
2015-10-05 20:36:27 -0700

    remove the old gmail image url hack

--- spec/omnicontacts/importer/gmail_spec.rb
+++ spec/omnicontacts/importer/gmail_spec.rb
@@ -165,7 +165,7 @@ describe OmniContacts::Importer::Gmail do
       result.last[:email].should eq("emilia.fox@gmail.com")
       result.last[:gender].should eq("female")
       result.last[:birthday].should eq({:day=>10, :month=>02, :year=>1974})
-      result.last[:profile_picture].should eq("https://profiles.google.com/s2/photos/profile/emilia.fox")
+      result.last[:profile_picture].should be_nil
       result.last[:relation].should eq('spouse')
       result.first[:address_1].should eq('1313 Trashview Court')
       result.first[:address_2].should eq('Apt. 13')
@@ -191,7 +191,7 @@ describe OmniContacts::Importer::Gmail do
       user[:email].should eq("chrisjohnson@gmail.com")
       user[:gender].should eq("male")
       user[:birthday].should eq({:day=>21, :month=>06, :year=>1982})
-      user[:profile_picture].should eq("https://profiles.google.com/s2/photos/profile/16482944006464829443")
+      user[:profile_picture].should eq("https://lh3.googleusercontent.com/-b8aFbTBM/AAAAAAI/IWA/vsek/photo.jpg")
     end
   end
 end
