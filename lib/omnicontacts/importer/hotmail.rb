f9770f16267ca2bd4e049ad0b755e1f7ec8ecec8
Kumaresan <kumaresan@railsfactory.org>
2016-01-06 20:03:16 +0530

    undefined local variable 'emails' Fix #151

--- lib/omnicontacts/importer/hotmail.rb
+++ lib/omnicontacts/importer/hotmail.rb
@@ -41,7 +41,7 @@ module OmniContacts
           # creating nil fields to keep the fields consistent across other networks
           contact = {:id => nil, :first_name => nil, :last_name => nil, :name => nil, :email => nil, :gender => nil, :birthday => nil, :profile_picture=> nil, :relation => nil, :email_hashes => []}
           contact[:id] = entry['user_id'] ? entry['user_id'] : entry['id']
-	        contact[:email] = parse_email(emails) if valid_email? parse_email(emails)
+	        contact[:email] = parse_email(entry['emails']) if valid_email? parse_email(entry['emails'])
 	        contact[:first_name] = normalize_name(entry['first_name'])
 	        contact[:last_name] = normalize_name(entry['last_name'])
 	        contact[:name] = normalize_name(entry['name'])
